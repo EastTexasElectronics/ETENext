70301f00c210023c0ac083d5d4e44a665c3f4d25
EastTexasElectronics <Contact@EastTexaselectronics.com>
2024-04-27 14:38:37 -0500

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Suspense } from 'react';
+import { SITE } from '~/config.js';
+import { Metadata } from 'next';
 import Loading from '../app/loading';
 import ThemeProviders from '~/components/atoms/ThemeProviders';
 import Footer from '~/components/widgets/Footer';
@@ -7,21 +9,67 @@ import { Inter as CustomFont } from 'next/font/google';
 import '~/assets/styles/base.css';
 // TODO Causing Hydration Error
 // import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
+// import { Analytics } from '@vercel/analytics/react';
 import BookingButton from '~/components/atoms/BookingButton';
 import { ClerkProvider } from '@clerk/nextjs';
 
 // Custom font
 const customFont = CustomFont({ subsets: ['latin'], variable: '--font-custom' });
 
-// TODO Import Metadata from src/shared/data/metadata/metadata.tsx
 // Metadata for the site
+export const metadata: Metadata = {
+  category: 'technology',
+  assets: ['https://EastTexasElectronics.com/Public_Assets'],
+  generator: 'Next.js',
+  applicationName: 'East Texas Electronics Support',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'IT Support',
+    'Computer Repair',
+    'Network Support',
+    'Web Design',
+    'SEO',
+    'Digital Marketing',
+    'East Texas',
+    'Tyler',
+    'Longview',
+    'Marshall',
+    'Henderson',
+    'Kilgore',
+  ],
+  authors: [{ name: 'Robert Havelaar', url: 'https://www.linkedin.com/in/robert-havelaar/' }],
+  creator: 'Robert Havelaar',
+  publisher: 'East Texas Electronics, LLC.',
+  title: {
+    template: `${SITE?.name ? `%s — ${SITE.name}` : 'East Texas Electronics'}`,
+    default: SITE?.title || 'East Texas Electronics',
+  },
+  description:
+    SITE?.description ||
+    'East Texas Electronics provides IT Support, Computer Repair, Network Support, Web Design, SEO, and Digital Marketing services to East Texas.',
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      {/* TODO remove className from HTML tag, this causes hydration issues. Find another way to implement.*/}
-      <html lang="en" className={`motion-safe:scroll-smooth 2xl:text-[24px] ${customFont.variable} font-sans`}>
+      <html
+        lang="en"
+        className={`motion-safe:scroll-smooth 2xl:text-[24px] ${customFont.variable} font-sans`}
+        suppressHydrationWarning
+      >
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -39,7 +87,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </html>
       {/* TODO Causing Hydration Error */}
       {/* <SpeedInsights /> */}
-      <Analytics />
+      {/* TODO Enable when ready to deploy no need to use yet. */}
+      {/* <Analytics /> */}
     </ClerkProvider>
   );
 }
